4b7f0757abe2a6205f12283ed4a88f1a5a0a2b86
Ashlyn Kennedy <ashbkenn@iu.edu>
2024-04-04 16:37:06 -0400

    Complete Backend

--- src/main/java/edu/iu/habahram/ducksservice/security/Jwks.java
+++ src/main/java/edu/iu/habahram/ducksservice/security/Jwks.java
@@ -1,6 +1,6 @@
 package edu.iu.habahram.ducksservice.security;
-
 import com.nimbusds.jose.jwk.RSAKey;
+//import java.security.interfaces.RSAKey;
 import java.security.KeyPair;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -13,7 +13,7 @@ public class Jwks {
         KeyPair keyPair = KeyGeneratorUtils.generateRsaKey();
         RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
         RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-        return new RSAKey().Builder(publicKey)
+        return new RSAKey.Builder(publicKey)
                 .privateKey(privateKey)
                 .keyID(UUID.randomUUID().toString())
                 .build();
